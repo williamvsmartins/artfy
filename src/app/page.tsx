b28bcfc5acc9b82c13420501c12c3bae05c55ff4
William Martins <williamvaltherprogramador@gmail.com>
2024-01-24 13:31:28 -0300

    feat: add sample tracks to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ import { MusicWallpaper } from '@/components/MusicWallpaper';
 import { Slider } from '@/components/Slider';
 import { SpotifySignInButton } from '@/components/SpotifySignInButton';
 
-import { authConfig } from '@/lib/auth';
+import SAMPLE_TRACKS from '../lib/sampleTracks.json';
 
+import { authConfig } from '@/lib/auth';
+import { TrackProps } from '@/services/http/topTrack/types';
 const words = ['Artfy'];
 const descriptions = [
   'Artfy transforma suas músicas mais ouvidas em Wallpapers aesthetics'
@@ -17,11 +19,14 @@ export default async function Page() {
 
   if (session) return redirect('/wallpaper');
 
+  const items: unknown = SAMPLE_TRACKS.items;
+  const convertedItems: TrackProps[] = items as TrackProps[];
+
   return (
     <div className="flex flex-col items-center p-6">
       <Slider words={words} descriptions={descriptions} />
       <SpotifySignInButton />
-      <MusicWallpaper />
+      {convertedItems && <MusicWallpaper tracks={convertedItems} />}
     </div>
   );
 }
