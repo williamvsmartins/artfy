8947e15c37b3e10daa4393fae43d5b5f499e6a71
William Martins <williamvaltherprogramador@gmail.com>
2024-01-26 21:36:21 -0300

    feat: add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col text-center">
+      <h1 className="text-4xl py-80 text-lime-300">Carregando...</h1>
+    </div>
+  );
+}
