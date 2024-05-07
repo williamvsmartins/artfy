bd975f696263c35ac7ceec7f9206cb1692fceaae
williamvsmartins <williamvaltherprogramador@gmail.com>
2024-05-07 16:05:56 -0300

    chore: add react query setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google';
 
 import { NextAuthProvider } from './providers';
 
+import TanstackProvider from '@/providers/TanstackProvider';
+
 import './globals.css';
 
 const font = Inter({ subsets: ['latin'] });
@@ -14,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br" className={`${font.className} bg-neutral-900`}>
       <body>
-        <NextAuthProvider>{children}</NextAuthProvider>
+        <TanstackProvider>
+          <NextAuthProvider>{children}</NextAuthProvider>
+        </TanstackProvider>
       </body>
     </html>
   );
---
+++ src/providers/TanstackProvider.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 12 * 60 * 60 * 1000 //12h
+      }
+    }
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === 'undefined') {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+const TanstackProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
